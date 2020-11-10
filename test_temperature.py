bfb00101cafbbb0e162ff7dd9f68192a28192531
Alois <wer.ders@gmx.de>
2020-11-10 21:17:16 +0100

    Test Temperatur

---
+++ test_temperature.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 11:03:15 2019
+
+@author: alois
+"""
+
+from temperature import in_celsius
+
+def test_temperature():
+    assert in_celsius(5) == print("48F = 5°C")
