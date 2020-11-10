6c1a097a8be071f3d6032b604776c7d01079a010
Alois <wer.ders@gmx.de>
2020-11-10 20:17:19 +0100

    temperatur umrechnung

---
+++ Temp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 11:03:15 2019
+
+@author: alois
+"""
+
+"""Aufgabe 2: Temperatur umrechnen"""
+
+print("Dieses Tool rechnet für Sie die Temperatur von °Fahrenheit in °Celsius um!")
+
+x= int(input("Geben Sie eine Temperatur in °Fahrenheit an: "))
+
+y=5/9*(x-32)
+
+print("y")
